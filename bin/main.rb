c121b7faece40fd100a648e6551a37101e4c0e30
Simon Peter Debbarma <31207418+simonpeterdebbarma@users.noreply.github.com>
2020-02-18 16:10:07 -0600

    :construction: create main flow of app; TODO: refactor

--- bin/main.rb
+++ bin/main.rb
@@ -2,96 +2,136 @@ require 'nokogiri'
 require 'httparty'
 require 'byebug'
 require 'json'
-require "tty-prompt"
+require 'tty-prompt'
 require 'nikkou'
 require 'tty-progressbar'
 require 'pastel'
 require 'tty-spinner'
+require 'tty-font'
+require 'io/console'  
 
 require_relative '../lib/shows.rb'
 
+@pastel = Pastel.new
+
 def list_parser
-    spinner = TTY::Spinner.new("[:spinner] Parsing EZTV", format: :dots, success_mark: '+')
+    spinner = TTY::Spinner.new('[:spinner] Parsing EZTV', format: :dots, success_mark: '+')
     spinner.auto_spin
     eztv = 'https://eztv.io/showlist/rating/'
     unparsed_showlist = HTTParty.get(eztv)
     @parsed_showlist = Nokogiri::HTML(unparsed_showlist.body)
-    spinner.success('(✔ Successful)')
-    spinner.error('(✖ Error)')
+    spinner.success(@pastel.blue('(✔ Successful)'))
+    spinner.error(@pastel.red('(✖ Error)'))
     puts ''
 end
 
 def showlist
     list_parser
     shows = []
-    pastel = Pastel.new
-    complete = pastel.on_green(" ")
-    incomplete = pastel.on_red(" ")
-    bar = TTY::ProgressBar.new("Grabbing your favorite shows! [:bar]", total: 10, width: 100, complete: complete, incomplete: incomplete)
-    (0...10).each do |x|
+    complete = @pastel.on_green(' ')
+    incomplete = @pastel.on_red(' ')
+    bar = TTY::ProgressBar.new('Grabbing your favorite shows [:bar]', total: 10, width: 50, complete: complete, incomplete: incomplete)
+    (0...30).each do |x|
         shows << show = @parsed_showlist.css('td.forum_thread_post')[x * 3].text
         bar.advance(1)
     end
-    sleep(2)
-    puts `clear`
+    puts ''
     shows
 end
 
-
 def select_show
     prompt = TTY::Prompt.new
-    show = prompt.select("Select a TV show?", showlist, filter: true, per_page: 20)
+    puts ''
+    show = prompt.select(@pastel.magenta(" Select a TV show?"), showlist, filter: true, per_page: 20)
     @series_link = 'https://eztv.io' + @parsed_showlist.search('a').text_includes(show).attribute('href').value
-    puts " Fantastic Choice! "
+    puts " Fantastic Choice!"
+    puts ''
     show_info
+    puts ''
+    episodes_info
+    puts ''
+    puts @pastel.green(' ~ Thank you for using eztv-scraper! Fork the project and build upon it! ~')
 end
 
 def parsed_show
+    spinner = TTY::Spinner.new("[:spinner] Searching the database!", format: :dots, success_mark: '+')
+    spinner.auto_spin
     url = @series_link
     unparsed_page = HTTParty.get(url)
     @parsed_page = Nokogiri::HTML(unparsed_page.body)
+    spinner.success(@pastel.yellow('(✔ Gottem)'))
+    spinner.error(@pastel.red('(✖ Something went wrong)'))
 end
 
 def show_info
     parsed_show
+    puts ''
+    spinner = TTY::Spinner.new("[:spinner] Grabbing show details", format: :dots, success_mark: '+')
+    spinner.auto_spin
     @output = []
     show_info = {
         title: @parsed_page.css('td.section_post_header span').text,
         description: @parsed_page.css('td.show_info_banner_logo p').text,
         poster: @parsed_page.css('td.show_info_main_logo').children.css('img').attribute('src').value,
-        imdb_rating: @parsed_page.css('td.show_info_rating_score div')[1].css('b span').text
-        }
+        imdb_rating: @parsed_page.css('td.show_info_rating_score div')[1].css('b span').text,
+    }
     @output << show_info
-    puts 'added info'
-    episodes_info
+    spinner.success(@pastel.yellow('(✔)'))
+    spinner.error(@pastel.red('(✖)'))
 end
 
 def episodes_info
-    itemcount = @parsed_page.css('tr.forum_header_border').count - 1
-    puts 'counted'
-    (0..itemcount).each do |x|
+    spinner = TTY::Spinner.new("[:spinner] Pulling magnets", format: :dots, success_mark: '+')
+    spinner.auto_spin
+    episodes = @parsed_page.css('tr.forum_header_border')
+    epcount = episodes.count - 1
+    epcount.times do |x|
         ep = {
-        episode_name: @parsed_page.css('tr.forum_header_border')[x].css('td.forum_thread_post')[1].css('a').text,
-        magnet: @parsed_page.css('tr.forum_header_border')[x].css('td.forum_thread_post')[2].css('a')[0].attribute('href').value,
-        size: @parsed_page.css('tr.forum_header_border')[x].css('td.forum_thread_post')[3].text,
-        released: @parsed_page.css('tr.forum_header_border')[x].css('td.forum_thread_post')[4].text,
-        seeds: @parsed_page.css('tr.forum_header_border')[x].css('td.forum_thread_post_end').text,
+        episode_name: episodes[x].css('td.forum_thread_post')[1].css('a').text,
+        magnet: episodes[x].css('td.forum_thread_post')[2].css('a')[0].attribute('href').value,
+        size: episodes[x].css('td.forum_thread_post')[3].text,
+        released: episodes[x].css('td.forum_thread_post')[4].text,
+        seeds: episodes[x].css('td.forum_thread_post_end').text,
         }
         @output << ep
     end
+    spinner.success(@pastel.yellow('(✔)'))
+    spinner.error(@pastel.red('(✖)'))
     json_output
 end
 
 def json_output
-    magnets = @output.to_json
-    local_fname = 'bb.json'
+    puts ''
+    spinner = TTY::Spinner.new("[:spinner] Writing to /eztv-scraped", format: :dots, success_mark: '+')
+    spinner.auto_spin
+    magnets = JSON.pretty_generate(@output)
+    page_title = @parsed_page.css('td.section_post_header span').text
+    Dir.mkdir("./eztv-scraped") unless Dir.exists? ("./eztv-scraped")
+    local_fname = './eztv-scraped/' + page_title + '.json'
     local_file = open(local_fname, 'w')
     local_file.write(magnets)
     local_file.close
+    spinner.success(@pastel.yellow('(✔)'))
+    spinner.error(@pastel.red('(✖)'))
 end
 
-# def redo
+def welcome
+    titlefont = TTY::Font.new(:starwars)
+    subtitlefont = TTY::Font.new(:straight)
+    puts @pastel.blue(titlefont.write(" EZTV",))
+    puts @pastel.blue(titlefont.write(" Scraper",))
+    puts @pastel.blue(titlefont.write("                                      v0.1.0",))
+    puts @pastel.red(subtitlefont.write(" Fork it on GitHub",))
+    puts @pastel.yellow(" https://github.com/simonpeterdebbarma/eztv-scraper")
+    continue_seq
+    select_show
+end       
 
-# end
+def continue_seq
+    puts ''
+    print " Press any key to continue..."                                                                                                    
+    STDIN.getch                                                                                                              
+    puts "\n"                                                                                           
+end 
 
-select_show
+welcome
--- lib/shows.rb
+++ lib/shows.rb
@@ -3,24 +3,5 @@ require 'httparty'
 require 'byebug'
 
 class Showlist
-    def showlist
-        url = 'https://eztv.io/showlist/'
-        unparsed_page = HTTParty.get(url)
-        parsed_page = Nokogiri::HTML(unparsed_page.body)
-        byebug
-        shows = []
-        # job_listings = parsed_page.css('li.js-job-opening')
-        # job_listings.each do |job_listing|
-        #   job = {
-        #     title: job_listing.css('h1.dst-job-openings__title').text,
-        #     url: job_listing.css('a.dst-button')[0].attributes['href'].value
-        #   }
-        #   jobs << job
-        # end
-        # puts jobs_json = jobs.to_json
-        # local_fname = 'doist-jobs.json'
-        # local_file = open(local_fname, 'w')
-        # local_file.write(jobs_json)
-        # local_file.close
-      end
+
 end
