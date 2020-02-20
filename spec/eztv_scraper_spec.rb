6b81e4086b730b202e713b8af7a68b5ed8f08c0b
Simon Peter Debbarma <31207418+simonpeterdebbarma@users.noreply.github.com>
2020-02-20 10:53:37 -0600

    :ok_hand: change invoke location from lib to bin
    more thanks to havenwood

--- spec/eztv_scraper_spec.rb
+++ spec/eztv_scraper_spec.rb
@@ -5,14 +5,7 @@ require_relative '../lib/eztv_scraper/shows.rb'
 require_relative '../lib/eztv_scraper/json.rb'
 
 describe EZTVScraper do
-  # describe 'list_parser' do
-  #     it 'begins the process' do
-  #         @pastel = Pastel.new
-  #         expect(welcome).to eq(true)
-  #     end
-
-  #     it 'does not begin the app' do
-  #         expect(welcome).to eq(false)
-  #     end
-  # end
+  before(:all) do
+    @EZTV = EZTVScraper.new
+  end
 end
