3a8ca625dc558da97a49f48af5f67a9bde5e51dd
Simon Peter Debbarma <31207418+simonpeterdebbarma@users.noreply.github.com>
2020-02-20 10:51:43 -0600

    write a before do in spec

--- spec/eztv_scraper_spec.rb
+++ spec/eztv_scraper_spec.rb
@@ -5,14 +5,9 @@ require_relative '../lib/eztv_scraper/shows.rb'
 require_relative '../lib/eztv_scraper/json.rb'
 
 describe EZTVScraper do
-  # describe 'list_parser' do
-  #     it 'begins the process' do
-  #         @pastel = Pastel.new
-  #         expect(welcome).to eq(true)
-  #     end
+  before(:all) do
+    @EZTV = EZTVScraper.new
+  end
 
-  #     it 'does not begin the app' do
-  #         expect(welcome).to eq(false)
-  #     end
-  # end
+  
 end
